dbdba9ceee99c1461a153dd00e8e9213e5809d89
Atefcohen <96726316+Atefcohen@users.noreply.github.com>
2022-01-10 21:53:20 +0200

    Add files via upload

---
+++ W3D2dailly/app.js
@@ -0,0 +1,17 @@
+//Exercise 1
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift();
+fruits.sort();
+fruits.push("Kiwi");
+fruits.splice(0,1);
+fruits.reverse();
+console.log(fruits);
+
+//Exercise 2
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(moreFruits[1][1][0]);
+
