f9d5f9d5bbcbaa6319137f96295c2f5fe568f8ee
Atefcohen <96726316+Atefcohen@users.noreply.github.com>
2022-02-16 19:16:29 +0200

    Add files via upload

---
+++ Week8/W8D2/dailly/app.js
@@ -0,0 +1,13 @@
+let form = document.querySelector('form')
+
+form.onsubmit = (e)=>{
+    console.log
+    e.preventDefault()
+    let data  = new FormData(e.target);
+    let obj = {}
+    for (var key of data.keys()) {
+		obj[key] = data.get(key);
+	}
+    console.log(JSON.stringify(obj))
+    
+}
